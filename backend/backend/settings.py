d4ada07ba9295bdd2f04982fa1ba4112e3a48461
VitendraBinated <Vitendra.Jangde@binated.com>
2026-01-07 16:31:43 +0530

    database-changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -84,16 +84,16 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django_demo_db',
-        'USER': 'django_user',
-        'PASSWORD': 'django_pass',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'django_demo_db',
+#         'USER': 'django_user',
+#         'PASSWORD': 'django_pass',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 
